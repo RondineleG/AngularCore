948f3a39c0b1d6deae5f3ffba17fc732a1ae5802
Rondineleg <rondineleg@edu.univali.br>
2020-09-04 19:29:25 -0300

    Classe de contexto para acesso a banco de dados

---
+++ back-end/src/MeusFilme.API/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MeusFilme.API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeusFilme.API.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Filme> Filme { get; set; }
+    }
+}
